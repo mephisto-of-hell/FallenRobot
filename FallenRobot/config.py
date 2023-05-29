6eea757791ef60a2f4dd26dd1f18ee4aa0cff153
mephisto-of-hell <125276079+mephisto-of-hell@users.noreply.github.com>
2023-05-29 14:53:44 +0530

    Update config.py
    Signed-off-by: mephisto-of-hell <125276079+mephisto-of-hell@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -16,7 +16,7 @@ class Config(object):
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://te.legra.ph/file/40eb1ed850cdea274693e.jpg"
 
-    SUPPORT_CHAT = "Mirchimusic123"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "OFFICIALBOT_SUPPORT"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = "6228294682:AAElZbOtQOsJ8NHM4mr2RxWNTITQWw2gwrw"  # Get bot token from @BotFather on Telegram
 
